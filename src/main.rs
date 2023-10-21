aeaf6ab10a6253471f033a05bcf630763576ce63
suap-dev <suap.dev@gmail.com>
2023-10-21 02:26:12 +0200

    Use env::args to specify folder

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
 use std::{
+    env,
     fs::{self, DirEntry},
     path::{Path, PathBuf},
 };
 
 fn main() {
-    let mut paths: Vec<PathBuf> = Vec::new();
+    let args: Vec<String> = env::args().collect();
+    let path = if args.len() < 2 {
+        ".".to_owned()
+    } else {
+        args[1].to_owned()
+    };
 
-    dig("INVALID_PATH", &mut paths);
+    let mut paths: Vec<PathBuf> = Vec::new();
+    dig(path, &mut paths);
 
     for p in &paths {
         println!("{}", p.display());
@@ -16,6 +23,21 @@ fn main() {
         let _ = fs::remove_dir_all(p);
     }
 }
+
+// The Rust community has developed guidelines for splitting the separate concerns of a binary program when main
+// starts getting large. This process has the following steps:
+// - Split your program into a main.rs and a lib.rs and move your program’s logic to lib.rs.
+// - As long as your command line parsing logic is small, it can remain in main.rs.
+// - When the command line parsing logic starts getting complicated, extract it from main.rs and move it to lib.rs.
+//
+// The responsibilities that remain in the main function after this process should be limited to the following:
+// - Calling the command line parsing logic with the argument values
+// - Setting up any other configuration
+// - Calling a run function in lib.rs
+// - Handling the error if run returns an error
+//
+// This pattern is about separating concerns: main.rs handles running the program, and lib.rs handles all the logic
+// of the task at hand.
 
 fn dig<P: AsRef<Path>>(path: P, paths: &mut Vec<PathBuf>) {
     for folder in sub_folders(path) {
